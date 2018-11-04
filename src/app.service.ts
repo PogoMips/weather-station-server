34e3a93a2f9e67873cda861a7a636a4e24f7b380
Sven Leonhardt <sven.leonhardt@sovanta.com>
2018-11-04 16:17:03 +0100

    return current data from sensor via /api/current

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,39 +3,58 @@ import { MockSensor } from './sensor/mock-sensor';
 
 const config = require('./configuration');
 
+export interface IDashboardData {
+    temperature: number;
+    temperatureLow: number;
+    temperatureHigh: number;
+    humidity: number;
+    humidityLow: number;
+    humidityHigh: number;
+}
+
 @Injectable()
 export class AppService {
     private sensor;
 
+    private temperature;
+    private humidity;
+
     constructor() {
+        this.initSensor();
+        this.readSensorData();
+    }
+
+    private initSensor() {
         try {
             this.sensor = require('node-dht-sensor');
         } catch (ex) {
             this.sensor = new MockSensor();
         }
-        this.readSensorData();
     }
 
     private readSensorData() {
         setInterval(() => {
-            this.sensor.read(22, 4, (err, temperature, humidity) => {
-                if (!err) {
-                    this.saveData(temperature, humidity);
-                } else {
-                    console.log('Cannot read from sensor', err);
-                }
-            });
+            this.sensor.read(22, 4, this.saveData);
         }, config.read_interval);
     }
 
-    private saveData(temperature, humidity) {
-        const temp = temperature.toFixed(1);
-        const hum = humidity.toFixed(1);
-        console.log('current temp:', temp);
-        console.log('current humidity:', hum);
-    }
+    saveData = (err, temperature, humidity) => {
+        if (!err) {
+            this.temperature = temperature;
+            this.humidity = humidity;
+        } else {
+            console.log('Cannot read from sensor', err);
+        }
+    };
 
-    root(): string {
-        return 'Hello World!';
-    }
+    getDashboardData = (): IDashboardData => {
+        return {
+            temperature: this.temperature,
+            humidity: this.humidity,
+            temperatureLow: 0,
+            temperatureHigh: 0,
+            humidityLow: 0,
+            humidityHigh: 0
+        };
+    };
 }
